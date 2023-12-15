87bd858248a94895071e83d00efe31ca3a4f4ca3
sapthashree15 <kssapthashree@gmail.com>
2023-12-15 03:05:10 -0500

    changes made from theia lab environment

--- src/config/database.js
+++ src/config/database.js
@@ -1,38 +1,13 @@
 const mongoose = require('mongoose');
-const User = require('../models/userModel')
+const User = require('../models/userModel');
 
 const connectDB = async () => {
-    try {
-      // Update your MongoDB connection string with authentication details
-     await mongoose.connect('mongodb://root:MTc1MzctbGF2YW55@localhost:27017');
-      console.log('MongoDB connected');
-
-    // Add sample data
-    const sampleData = [
-      {
-        username: 'john_doe',
-        password: 'password123',
-      },
-      {
-        username: 'jane_smith',
-        password: 'pass456',
-      },
-      {
-        username: 'alex_jones',
-        password: 'secret789',
-      },
-    ];
-
-    await User.insertMany(sampleData);
-    console.log('Sample data added to the database');
-
-    // Close the MongoDB connection
-    mongoose.connection.close();
-    console.log('MongoDB connection closed');
+  try {
+    await mongoose.connect('mongodb://root:OTM3MS1zYXB0aGFz@localhost:27017');
+    console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
-
 module.exports = connectDB;
